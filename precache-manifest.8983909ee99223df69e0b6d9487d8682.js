dcb9185f036a50563347b09b354b63305a38637f
Roman Agarkov <agarkovromachka@mail.ru>
2020-06-03 23:29:17 +0300

    Updates

---
+++ precache-manifest.8983909ee99223df69e0b6d9487d8682.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0572ffc92d479e2fc5f38467daa03d06",
+    "url": "/react-quiz-app/index.html"
+  },
+  {
+    "revision": "c41bd3996363ba510ff8",
+    "url": "/react-quiz-app/static/css/main.c26ea452.chunk.css"
+  },
+  {
+    "revision": "d1bbd4de71c19f8daaaa",
+    "url": "/react-quiz-app/static/js/2.c7a91edf.chunk.js"
+  },
+  {
+    "revision": "fae6e97322ddc0579046aa829b382727",
+    "url": "/react-quiz-app/static/js/2.c7a91edf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c41bd3996363ba510ff8",
+    "url": "/react-quiz-app/static/js/main.b89e9453.chunk.js"
+  },
+  {
+    "revision": "303017225e993e37be5c",
+    "url": "/react-quiz-app/static/js/runtime-main.2074bfc4.js"
+  }
+]);
